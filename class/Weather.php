f06daabe0d1cdcbfd701d8e256deb69e8501972e
1999matrix <shashwatsd44@gmail.com>
2023-07-23 18:17:23 +0530

    changes i api call  , added weather.php

--- class/Weather.php
+++ class/Weather.php
@@ -1,15 +1,37 @@
-<?php
-class ApiCall{
-  
-  protected function apiCall(string $url)
+
+
+  <?php
+declare(strict_types=1);
+require 'ApiCall.php';
+
+class Weather extends ApiCall{
+  private string $ip;
+  private array $details;
+  public function __construct(string $ip){
+    $this->ip = $ip;
+  }
+    public function load()
   {
-    $curl = curl_init();
-    curl_setopt($curl,CURLOPT_URL,$url);
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-    $data = curl_exec($curl);
-    $apidata = json_decode($data);
-    return $apidata;
+    return $this->details;
+    
   }
-  
+  public function getInfo():?string
+  {
+    $url = 'https://api.ipfind.com/?ip='.$this->ip.'';
+    $apidata = $this->apiCall($url);
+   $longitude = $apidata->longitude;
+   $latitude = $apidata->latitude;
+   return $this->getWeather($latitude,$longitude);
   }
+  private function getWeather(int|float $latitude,int|float $longitude):void
+  {
+    //to get api key go https://openweathermap.org
+    $apikey = 'API KEY HERE';
+    $url = 'https://api.openweathermap.org/data/2.5/weather?lat='.$latitude.'&lon='.$longitude.'&appid='.$apikey.'&units=metric';
+    
+    $apidata = $this->apiCall($url);
+    $this->details[] = $apidata;
+  }
+
+}
 //   b59d35bda22d0eaf6ad699b7e11c0114
